4cc994f6eff79bd0c2d108c7cbaaa8f7759b9597
andrewlalis <andrewlalisofficial@gmail.com>
2019-09-13 09:54:39 +0200

    Removed unused imports.

--- src/main/java/nl/andrewlalis/Main.java
+++ src/main/java/nl/andrewlalis/Main.java
@@ -13,8 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * The main program entry point for this application.
